51f286e2d5429b4f0077545ba4c61b6876b1a755
Wasif Khan <waasif.khaan@gmail.com>
2018-11-12 19:18:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function getRepositories() {
+<script>function getRepositories() {
   const req = new XMLHttpRequest();
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+</script>
