df0d1e928da16ea1b90fe948fbdea7c4ecc14ff8
Wasif Khan <waasif.khaan@gmail.com>
2018-11-12 19:31:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function getRepositories() {
 }
 
 function showRepositories() {
-  //this is set to the XMLHttpRequest object that fired the event
   console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
